2f11f5742e79c4995c38ddc99c15ed181f1515ac
KonradK <konrad.0808.k@gmail.com>
2023-01-28 03:08:46 +0100

    Fix js and update styles.

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ var brList = document.querySelectorAll("br");
 // FUNCTION DECLARATIONS
 function createListElement() {
 	//add break line and create li element with text depending on input value
-	ul.appendChild(document.createElement("br"));
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	//create button element
 	var deleteButton = document.createElement("button");
 	deleteButton.appendChild(document.createTextNode("delete"));
-	deleteButton.setAttribute('id','delete');
+	deleteButton.setAttribute('id', 'delete');
 	ul.appendChild(deleteButton);
+	ul.appendChild(document.createElement("br"));
 	//clear input
 	input.value = "";
 	//update node lists
@@ -42,18 +42,18 @@ function addListAfterKeypress(event) {
 // EVENTS
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
-ul.addEventListener("click", function(e) {
-	if(e.target.tagName === "LI") {
+ul.addEventListener("click", function (e) {
+	if (e.target.tagName === "LI") {
 		e.target.classList.toggle("done"); // on/off "done" class
 	}
-	else if(e.target.id === "delete") {
+	else if (e.target.id === "delete") {
 		//change nodelist into array and get index of delete button
 		var deleteArray = Array.from(deleteList);
 		var buttonIndex = deleteArray.indexOf(e.target);
 		//remove br, li and delete button
 		liList[buttonIndex].remove();
 		deleteList[buttonIndex].remove();
-		brList[buttonIndex-1].remove();
+		brList[buttonIndex].remove();
 		//update node lists
 		liList = document.querySelectorAll("li");
 		deleteList = document.querySelectorAll("#delete");
