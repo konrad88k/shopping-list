ce70e427d25b59643b09ce497c1aebaff429b16b
konrad88k <konrad.0808.k@gmail.com>
2023-03-08 21:36:27 +0100

    Fix script. Change delete button style.

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 // INITIAL VARIABLES
-var button = document.getElementById("enter-btn");
 var input = document.getElementById("userinput");
+var button = document.getElementById("enter-btn");
 var ul = document.querySelector("ul");
 var liList = document.querySelectorAll("li");
 var deleteList = document.querySelectorAll("#delete");
-var brList = document.querySelectorAll("br");
 
 // FUNCTION DECLARATIONS
 function createListElement() {
-	//add break line and create li element with text depending on input value
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	//create button element
-	var deleteButton = document.createElement("button");
+	// Create li element with checkbox, label and delete button / text depending on input value
+	let li = document.createElement("li");
+	let label = document.createElement("label");
+	label.appendChild(document.createTextNode(input.value));
+	label.setAttribute('class', 'checkbox-container');
+	let checkbox = document.createElement("input");
+	checkbox.setAttribute('type', 'checkbox');
+	label.appendChild(checkbox);
+	let checkmark = document.createElement("span");
+	checkmark.setAttribute('class', 'checkmark');
+	label.appendChild(checkmark);
+	li.appendChild(label);
+	let deleteButton = document.createElement("button");
 	deleteButton.appendChild(document.createTextNode("delete"));
 	deleteButton.setAttribute('id', 'delete');
-	ul.appendChild(deleteButton);
-	ul.appendChild(document.createElement("br"));
-	//clear input
+	li.appendChild(deleteButton);
+	ul.appendChild(li);
+	// Clear input
 	input.value = "";
-	//update node lists
+	// Update node lists
 	liList = document.querySelectorAll("li");
 	deleteList = document.querySelectorAll("#delete");
-	brList = document.querySelectorAll("br");
-}
-function inputLength() {
-	return input.value.length;
 }
 function addListAfterClick() {
-	if (inputLength() > 0) {
+	if (input.value.length > 0) {
 		createListElement();
 	}
 }
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
+	if (input.value.length > 0 && event.keyCode === 13) {
 		createListElement();
 	}
 }
@@ -43,20 +45,18 @@ function addListAfterKeypress(event) {
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
 ul.addEventListener("click", function (e) {
-	if (e.target.tagName === "LI") {
-		e.target.classList.toggle("done"); // on/off "done" class
-	}
-	else if (e.target.id === "delete") {
-		//change nodelist into array and get index of delete button
+	if (e.target.tagName === "LABEL") {
+		e.target.parentNode.children[0].classList.toggle("done"); // on/off "done" class
+	} else if (e.target.tagName === "SPAN") {
+		e.target.parentNode.classList.toggle("done");
+	} else if (e.target.id === "delete") {
+		// Change nodelist into array and get index of delete button
 		var deleteArray = Array.from(deleteList);
 		var buttonIndex = deleteArray.indexOf(e.target);
-		//remove br, li and delete button
+		// Remove li
 		liList[buttonIndex].remove();
-		deleteList[buttonIndex].remove();
-		brList[buttonIndex].remove();
-		//update node lists
+		// Update node lists
 		liList = document.querySelectorAll("li");
 		deleteList = document.querySelectorAll("#delete");
-		brList = document.querySelectorAll("br");
 	}
 });
